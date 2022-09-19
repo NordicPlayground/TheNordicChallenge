b8bc2ff0172190f838649fc8a5d1334478c83f47
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2022-09-19 14:43:16 +0200

    fix: clone summary when changing points, and add sorting in function

--- src/utils/hourlyPoints.ts
+++ src/utils/hourlyPoints.ts
@@ -1,13 +1,34 @@
-export const HourlyPoints = (weeklyHoursSorted: any, summary: any): void => {
+export type SummaryData = {
+	name: string
+	distance: number
+	hours: number
+	clubPoints: number
+	elevation: number
+}[]
+
+export const HourlyPoints = (
+	weeklyHoursSorted: SummaryData,
+	summary: SummaryData,
+): SummaryData => {
+	const summaryData: SummaryData = summary.map((s) => ({ ...s }))
+	weeklyHoursSorted = (weeklyHoursSorted ?? []).sort(
+		(a: { hours: number }, b: { hours: number }) => b.hours - a.hours,
+	)
 	const weeklyHoursSliced = weeklyHoursSorted.slice(0, 4)
-	console.log(summary)
+
 	let points = 4
-	for (const clubs of weeklyHoursSliced) {
-		for (const summaryClubs of summary) {
-			if (clubs.name === summaryClubs.name) {
-				summaryClubs.clubPoints += points
+	for (const club of weeklyHoursSliced) {
+		for (const summaryClub of summaryData) {
+			if (club.name === summaryClub.name) {
+				summaryClub.clubPoints += points
 			} else continue
+
 			points -= 1
 		}
 	}
+	const sortedDataForGraph = (summaryData ?? []).sort(
+		(a: { clubPoints: number }, b: { clubPoints: number }) =>
+			b.clubPoints - a.clubPoints,
+	)
+	return sortedDataForGraph
 }
