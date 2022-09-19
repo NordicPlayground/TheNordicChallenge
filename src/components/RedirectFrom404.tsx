dba2181dde004a90f28499f39256e79d438682b5
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2022-09-19 16:28:03 +0200

    fix: only redirect once

--- src/components/RedirectFrom404.tsx
+++ src/components/RedirectFrom404.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const redirect = new URLSearchParams(window.location.search).get('redirect')
@@ -7,11 +7,20 @@ const redirect = new URLSearchParams(window.location.search).get('redirect')
  * Navigate to the desired page in case it was requested by the user, but GitHub pages served a 404 page (because it does not know about our apps routes).
  */
 export const RedirectFrom404 = () => {
+	const [redirected, setRedirected] = useState<boolean>(false)
+
 	const navigate = useNavigate()
+
 	useEffect(() => {
 		if (redirect === null) return
+		if (redirected) {
+			// Already redirected
+			return
+		}
+		setRedirected(true)
+		console.log(`Redirecting to`, redirect)
 		navigate(redirect)
-	}, [navigate])
+	}, [navigate, redirected])
 
 	return null
 }
