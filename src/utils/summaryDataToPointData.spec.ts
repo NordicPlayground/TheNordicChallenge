6b33a5b42ac5ea799edcae2d1c977cc3f9e13d52
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2022-09-19 10:45:32 +0200

    feat: add function and test for turning summaries to point data

--- src/utils/summaryDataToPointData.spec.ts
+++ src/utils/summaryDataToPointData.spec.ts
@@ -1,93 +1,59 @@
-import { summaryDataToPointData } from './summaryDataToPointData'
+import { summaryDataToPointData } from 'utils/summaryDataToPointData.js'
 
 describe('summaryDataToPointData()', () => {
 	it('should convert summary data to point data', () => {
-		const summaryWeek1 = {
-			timestamp: 1663324192,
-			totalData: {
-				totalDistance: 1063.7,
-				totalHours: 156.8,
-				totalPoints: 12,
-			},
-			summary: [
-				{
-					name: 'Nordic Semiconductor - Trondheim Office',
-					distance: 169.60000000000005,
-					hours: 29.799999999999997,
-					clubPoints: 1.4,
-					elevation: 4352.8,
+		const summaries = [
+			{
+				timestamp: 1663324192,
+				totalData: {
+					totalDistance: 1063.7,
+					totalHours: 156.8,
+					totalPoints: 12,
 				},
-				{
-					name: 'Nordic Semiconductor - Oslo Office',
-					distance: 70.39999999999999,
-					hours: 13.199999999999998,
-					clubPoints: 0.7,
-					elevation: 1160.1999999999998,
-				},
-			],
-		}
-		const summaryWeek2 = {
-			timestamp: 1662938806,
-			totalData: {
-				totalDistance: 1063.7,
-				totalHours: 156.8,
-				totalPoints: 12,
+				summary: [
+					{
+						name: 'Nordic Semiconductor - Trondheim Office',
+						distance: 169.60000000000005,
+						hours: 29.799999999999997,
+						clubPoints: 1.4,
+						elevation: 4352.8,
+					},
+					{
+						name: 'Nordic Semiconductor - Oslo Office',
+						distance: 70.39999999999999,
+						hours: 13.199999999999998,
+						clubPoints: 0.7,
+						elevation: 1160.1999999999998,
+					},
+				],
 			},
-			summary: [
-                {
-                  name: "Nordic Semiconductor - Trondheim Office",
-                  distance: 169.60000000000005,
-                  hours: 29.799999999999997,
-                  clubPoints: 4,
-                  elevation: 4352.8
-                },
-                {
-                  name: "Nordic Semiconductor - Oslo Office",
-                  distance: 70.39999999999999,
-                  hours: 13.199999999999998,
-                  clubPoints: 3.7,
-                  elevation: 1160.1999999999998
-                }
-              ]
-		}
-		/*const summaryDataWeek38 = 
-        [
-            {
-              name: "Nordic Semiconductor - Trondheim Office",
-              distance: 169.60000000000005,
-              hours: 29.799999999999997,
-              clubPoints: 1.4,
-              elevation: 4352.8
-            },
-            {
-              name: "Nordic Semiconductor - Oslo Office",
-              distance: 70.39999999999999,
-              hours: 13.199999999999998,
-              clubPoints: 0.7,
-              elevation: 1160.1999999999998
-            }
-          ]
-    const summaryDataWeek39 = 
-        [
-            {
-              name: "Nordic Semiconductor - Trondheim Office",
-              distance: 169.60000000000005,
-              hours: 29.799999999999997,
-              clubPoints: 4,
-              elevation: 4352.8
-            },
-            {
-              name: "Nordic Semiconductor - Oslo Office",
-              distance: 70.39999999999999,
-              hours: 13.199999999999998,
-              clubPoints: 3.7,
-              elevation: 1160.1999999999998
-            }
-          ]*/
+			{
+				timestamp: 1663568223,
+				totalData: {
+					totalDistance: 1063.7,
+					totalHours: 156.8,
+					totalPoints: 12,
+				},
+				summary: [
+					{
+						name: 'Nordic Semiconductor - Trondheim Office',
+						distance: 169.60000000000005,
+						hours: 29.799999999999997,
+						clubPoints: 4,
+						elevation: 4352.8,
+					},
+					{
+						name: 'Nordic Semiconductor - Oslo Office',
+						distance: 70.39999999999999,
+						hours: 13.199999999999998,
+						clubPoints: 3.7,
+						elevation: 1160.1999999999998,
+					},
+				],
+			},
+		]
 
-		expect(
-			summaryDataToPointData(summaryWeek1, summaryWeek2),
-		).toMatchObject([
+		expect(summaryDataToPointData(summaries)).toMatchObject([
 			{
 				week: 38,
 				club: 'Trondheim',
--- src/utils/summaryDataToPointData.ts
+++ src/utils/summaryDataToPointData.ts
@@ -1,53 +1,50 @@
-import type { StravaObject } from "app/pages/Strava"
-import { getWeek } from "date-fns"
-import type { PointData } from "./pointData2GraphData"
+import type { StravaObject } from 'app/pages/Strava'
+import { getWeek } from 'date-fns'
+import type { PointData } from 'utils/pointData2GraphData.js'
 
-export type SummaryData = 
-    {
-      name: string
-      distance: number,
-      hours: number,
-      clubPoints: number,
-      elevation: number
-    }[]
-  
+export type SummaryData = {
+	name: string
+	distance: number
+	hours: number
+	clubPoints: number
+	elevation: number
+}[]
 
-export const summaryDataToPointData = (data1: StravaObject, data2: StravaObject): PointData => {
+export const summaryDataToPointData = (
+	data: StravaObject[],
+): PointData | undefined => {
 	const result: PointData = [] as any
-    const weekNumber = getWeek(data1.timestamp)
-    console.log('weeknumber',weekNumber)
-    /*
-    //First summary can be added directly
-    for (const officeData of data1){
-        let clubName = officeData.name.split('- ').pop()
-        if (clubName?.includes('Office')){
-            clubName = clubName.slice(0,-7)
-        }
-        result.push({
-            week: 38,
-            club: clubName as string,
-            points: officeData.clubPoints
-        })
-    }
-    //Second and later summaries
-    for (const officeDataWeek2 of data2){
-        let clubName = officeDataWeek2.name.split('- ').pop()
-        if (clubName?.includes('Office')){
-            clubName = clubName.slice(0,-7)
-        }
-        let oldPoints = 0
-        for (const res of result){
-            if (res.club === clubName){
-                oldPoints += res.points
-            }
-        }
-        result.push({
-            week: 39,
-            club: clubName as string,
-            points: officeDataWeek2.clubPoints + oldPoints
-        })
-        oldPoints = 0
-    }
-    console.log(result)*/
+	if (data === undefined) {
+		return
+	}
+	for (const week of data) {
+		const weekNumber = getWeek(week.timestamp * 1000)
+		for (const officeData of week.summary) {
+			let clubName = officeData.name.split('- ').pop()
+			if (clubName === undefined) {
+				console.log('Clubname undefined')
+				return
+			}
+			if (clubName?.includes('Office')) {
+				clubName = clubName.slice(0, -7)
+			}
+			if (clubName?.includes('NTNU')) {
+				clubName = clubName.slice(0, -5)
+			}
+			let oldPoints = 0
+			for (const res of result) {
+				if (res.club === clubName) {
+					oldPoints += res.points
+				}
+			}
+			result.push({
+				week: weekNumber,
+				club: clubName,
+				points: officeData.clubPoints + oldPoints,
+			})
+			oldPoints = 0
+		}
+	}
+
 	return result
 }
