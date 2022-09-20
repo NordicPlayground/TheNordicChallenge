a197214ba86dd0cdfc744a5f14b5164a1896dc61
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2022-09-20 08:36:23 +0200

    fix: use ISO 8601 convention for week numbers (#1)

--- src/utils/getWeek.ts
+++ src/utils/getWeek.ts
@@ -2,5 +2,8 @@ import { getWeek } from 'date-fns'
 
 export const weekNumber = getWeek(new Date(), {
 	weekStartsOn: 1,
-	firstWeekContainsDate: 1,
+	/* firstWeekContainsDate is set as per ISO 8601. This is valid for EU (exc. Portugal)
+	 * and most of other European countries, most of Asia and Oceania.
+	 * See https://en.wikipedia.org/wiki/Week#Week_numbering . */
+	firstWeekContainsDate: 4,
 })
