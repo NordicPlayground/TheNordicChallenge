be97df42f6034af2193ae56f2b4fb1528d2675a9
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-01-30 15:34:11 +0100

    fix: fetch weekdata array and exp time from cache

--- src/utils/fetchWeekData.ts
+++ src/utils/fetchWeekData.ts
@@ -1,12 +1,26 @@
-export const fetchWeekData = async () => {
-    const weekData = []
-    for(let weekNum = 4; weekNum<10; weekNum++) {
-        try {
-            const res = await fetch(`https://lenakh97.github.io/Nordic-strava-application/summary-week-${weekNum.toString().padStart(2, '0')}.json?`)
-            weekData.push(await res.json())
-        } catch {
-            // Pass, week does not exist
-        }
-    }
-    return weekData
+import type { StravaObject } from 'app/pages/Strava'
+
+export const fetchWeekData = async (): Promise<{
+	weekData: StravaObject[]
+	exp: number | undefined
+}> => {
+	const weekData = []
+	let exp
+	for (let weekNum = 4; weekNum < 8; weekNum++) {
+		try {
+			const res = await fetch(
+				`https://lenakh97.github.io/Nordic-strava-application/summary-week-${weekNum
+					.toString()
+					.padStart(2, '0')}.json?`,
+			)
+			weekData.push(await res.json())
+			exp = parseInt(
+				res?.headers.get('cache-control')?.split('=')?.[1] ?? '3600',
+				10,
+			)
+		} catch {
+			// Pass, week does not exist
+		}
+	}
+	return { weekData, exp }
 }
